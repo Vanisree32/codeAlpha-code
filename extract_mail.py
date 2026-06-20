30ce470bef656f2440d9a3b0bccc4251f9e39839
Vanisree32 <bandavidyavanisri@gmail.com>
2026-06-20 17:37:57 +0530

    Emails Application

---
+++ extract_mail.py
@@ -0,0 +1,20 @@
+import re
+
+with open("input.txt", "r") as file:
+    text = file.read()
+
+emails = re.findall(
+    r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}",
+    text
+)
+
+with open("emails.txt", "w") as file:
+    for email in emails:
+        file.write(email + "\n")
+
+print("Emails extracted successfully!")
+
+print("\nFound Emails:")
+
+for email in emails:
+    print(email)
